8f64a83f6513806be95866ad579353ab48fa60b6
Maksim Kazantsev <maksimkazantsev2003@gmail.com>
2024-01-30 21:27:51 +0300

    v5

--- cmd/main.go
+++ cmd/main.go
@@ -5,34 +5,16 @@ import (
 	"fmt"
 	"github.com/MaksKazantsev/go-crud/internal/helper"
 	"github.com/MaksKazantsev/go-crud/internal/model"
+	"github.com/gorilla/mux"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 // Book struct with ID, Title, Author with string DT
 // Declaring a variable called books, with book DT
 var books []model.Book
-
-// This is a router function, because i was trying not to use libs, i had to write a switch and case to check if the method is right,
-// and if it is, than we are calling a function for each case
-func mainFunc(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		switch r.URL.Path {
-		case "/books/":
-			getBooks(w, r)
-		default:
-			getBook(w, r)
-		}
-	case http.MethodPost:
-		createBook(w, r)
-	case http.MethodPut:
-		updateBook(w, r)
-	case http.MethodDelete:
-		deleteBook(w, r)
-	}
-}
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")       // Setting up a Content-Type - json
@@ -43,16 +25,16 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 
 func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	id := strings.TrimPrefix(r.URL.Path, "/books/")
-	if strings.ContainsAny(id, "0123456789") {
-		for _, item := range books {
-			if fmt.Sprintf("%d", item.ID) == id {
+	params := mux.Vars(r)
+	for _, item := range books {
+		if strings.ContainsAny(params["ID"], "0123456789") {
+			if item.ID == params["ID"] {
 				json.NewEncoder(w).Encode(item)
 				return
 			}
+		} else {
+			http.Error(w, "Error - no string in URL!", http.StatusBadRequest)
 		}
-	} else {
-		w.WriteHeader(http.StatusBadRequest)
 	}
 }
 
@@ -60,37 +42,37 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var bookItem model.Book
 	_ = json.NewDecoder(r.Body).Decode(&bookItem)
-	bookItem.ID = rand.Intn(100000)
+	bookItem.ID = strconv.Itoa(rand.Intn(100000))
 	books = append(books, bookItem)
 	json.NewEncoder(w).Encode(bookItem)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	id := strings.TrimPrefix(r.URL.Path, "/books/")
-	if strings.ContainsAny(id, "0123456789") {
+	params := mux.Vars(r)
+	if strings.ContainsAny(params["ID"], "0123456789") {
 		for index, item := range books {
-			if fmt.Sprintf("%d", item.ID) == id {
-				var body model.Book
-				if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-				body.ID = item.ID
-				books[index] = body
-				json.NewEncoder(w).Encode(books)
+			if item.ID == params["ID"] {
+				books = append(books[:index], books[index+1:]...)
+				var book model.Book
+				_ = json.NewDecoder(r.Body).Decode(&book)
+				book.ID = params["ID"]
+				books = append(books, book)
+				json.NewEncoder(w).Encode(book)
 				return
 			}
 		}
+	} else {
+		http.Error(w, "Error - no string in URL!", http.StatusBadRequest)
 	}
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	id := strings.TrimPrefix(r.URL.Path, "/books/")
-	if strings.ContainsAny(id, "0123456789") {
+	params := mux.Vars(r)
+	if strings.ContainsAny(params["ID"], "0123456789") {
 		for index, item := range books {
-			if fmt.Sprintf("%d", item.ID) == id {
+			if item.ID == params["ID"] {
 				books = append(books[:index], books[index+1:]...)
 				break
 			}
@@ -99,26 +81,24 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Creating a value method for handler struct, basically this method serves an HTTP response and request and then colling a router function
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	mainFunc(w, r)
-}
-
-// Handler Struct
-type handler struct{}
-
 func main() {
 	fmt.Println("Starting server!")
-	// Appending an example (not required)
-	books = append(books, model.Book{ID: 1, Title: "Example", Author: "Example Author"})
+	r := mux.NewRouter()
+	r.HandleFunc("/books/", getBooks).Methods("GET")
+	r.HandleFunc("/books/{ID}", getBook).Methods("GET")
+	r.HandleFunc("/books/", createBook).Methods("POST")
+	r.HandleFunc("/books/{ID}", deleteBook).Methods("DELETE")
+	r.HandleFunc("/books/{ID}", updateBook).Methods("PUT")
 
-	// Setting up some stuff about HTTP server, such as Address and HandlerFunction
+	books = append(books, model.Book{ID: "1", Title: "Example", Author: "Example Author"})
+
 	srv := http.Server{
 		Addr:    ":8000",
-		Handler: handler{},
+		Handler: r,
 	}
 
 	// Launching server, using srv struct and handling an error
+
 	err := srv.ListenAndServe()
 	helper.PanicIfErr(err)
 }
